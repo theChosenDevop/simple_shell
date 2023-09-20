57621e24ec8373f4488863324d8cde678daeb4c8
theChosenDevop <tobbey.adesanya@gmail.com>
2023-09-20 14:14:05 -0700

    custom strtok

--- split_tokens.c
+++ split_tokens.c
@@ -12,11 +12,63 @@ void split_line_tokens(char *lineptr, char **argv)
 	const char *delim = " \n\t";
 	int i = 0;
 
-	token = strtok(lineptr, delim);
+	token = _strtok(lineptr, delim);
 	while (token != NULL)
 	{
 		argv[i++] = _strdup(token);
-		token = strtok(NULL, delim);
+		token = _strtok(NULL, delim);
 	}
 	argv[i] = NULL;
+}
+
+/**
+ * _strtok - tokenize strings
+ * @string: input
+ * @del: delimiter
+ * Return: pointer to the next token
+ */
+
+char *_strtok(char *string, const char *del)
+{
+	static char *tok;
+
+	char *new_token = NULL;
+
+	if (string != NULL)
+		tok = string;
+	if (tok == NULL || *tok == '\0')
+		return (NULL);
+	while (*tok != '\0' && compare_str_delim(*tok, del))
+		tok++;
+	if (*tok == '\0')
+		return (NULL);
+
+	new_token = tok;
+
+	while (*tok != '\0' && !compare_str_delim(*tok, del))
+		tok++;
+	if (*tok != '\0')
+	{
+		*tok = '\0';
+		tok++;
+	}
+
+	return (new_token);
+}
+
+/**
+ * compare_str_delim - checks if the delimiter and character match
+ * @a: input to check
+ * @del: characters to compare
+ * Return: 1 on success, 0 on failure
+ */
+unsigned int compare_str_delim(char a, const char *del)
+{
+	while (*del != '\0')
+	{
+		if (*del == a)
+			return (1);
+		del++;
+	}
+	return (0);
 }
